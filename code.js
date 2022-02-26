380d8f080a446cd3a4adbb9a81a9a10e24b14a56
Alfonzo Suarez <samuelalfonzo2@hotmail.com>
2022-02-26 18:51:19 -0430

    ITS ALIVEEEE

--- code.js
+++ code.js
@@ -18,41 +18,37 @@ class Billete
 function entregarDinero()
 {
    for(bi of caja)
+   {
+    if(dinero > 0)
     {
-       if(dinero > 0)
-       {
-           // aca se saca la cuenta de la caja, se hacen todas las operaciones //
-            div = Math.floor(dinero / bi.valor);
-
-            if(div > bi.cantidad)
-             {
-                 papeles = bi.cantidad;
-             }
-
-             else
-             {
-                 papeles = div;
-             }
-             entregado.push(new Billete(bi.valor, papeles));
-             dinero = dinero - (bi.valor * papeles);
-            console.log(entregado);
-        }     
-     }
-
-     if(dinero > 0)
-     {
-         resultado.innerHTML = "No puedo darte esa cantidad";
-     }
-     else 
-     {
-         for(e of entregado)
-         {
-             if(e.cantidad > 0)
-             {
-                resultado.innerHTML += e.cantidad + " billetes de $" + e.valor + "<br />";
-             }
-         }
-     }
+        div = Math.floor(dinero / bi.valor)
+        if(div > bi.cantidad)
+        {
+            papeles = bi.cantidad
+        }
+        else
+        {
+            papeles = div;
+        }
+        dinero = dinero - (papeles * bi.valor);
+        entregado.push(new Billete(papeles, bi.valor));
+     } 
+   }
+   if(dinero > 0)
+   {
+       resultado.innerHTML = "No tengo suficiente dinero <br/>";
+       
+   }
+   else
+   {
+      for(e of entregado)
+      {
+          if(e.cantidad > 0)
+          {
+              resultado.innerHTML += e.cantidad + " billetes de " + e.valor + "$ <br/>";
+          }
+      }
+   }
 
 }
      
@@ -61,13 +57,13 @@ function entregarDinero()
 var entregado = []; /* array de los billetes entregados */
 var caja = []; /* array de los billetes disponibles */
 
-caja.push(new Billete(5,100));
-caja.push(new Billete(10,50));
-caja.push(new Billete(5,20));
-caja.push(new Billete(10,10));
-caja.push(new Billete(10,5));
+
+caja.push(new Billete(3,50));
+caja.push(new Billete(1,20));
+caja.push(new Billete(1,10));
+caja.push(new Billete(1,5));
 
 
 var div;
-var papeles;
+var dineroEntregado;
 
